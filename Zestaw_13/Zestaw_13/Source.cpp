e57b6ee8365436739dfb12a615af52edecd445f0
Paweł <pawel.sapek@gmail.com>
2017-01-31 23:51:55 +0100

    Start

---
+++ Zestaw_13/Zestaw_13/Source.cpp
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+Present your individual project to the teacher, ensure that everything is correct and you may write a report.
+Write a program generating 6 random numbers of a lottery drawing(6 of 49).Run it twice.
+Write a program reading 5 integers into an array and sorting them using the qsort() function.
+Read another, sixth integer from the user and check whether it is one of these five.Use bsearch() on a sorted array.
+Check, how many floating - point multiplications the computer performs in a second.Hint: multiply random numbers for a given amount of time : wait, until time() changes, then multiply numbers until it changes again(which should happen in one second).Second hint : check, how long does it take to execute an empty loop and don’t take this time into account – measure only the time of multiplications.Repeat the measurement with divisions.
+Prepare well for the written test on Monday.
